e910aaaf67f312663d1f7c8f52e83c227dab45f6
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2024-10-24 16:42:25 +0530

    removed toast componenet from the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,28 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import { ToastContainer, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 
 const App = () => {
     return (
         <Router>
-            <div className='min-h-screen w-full'>
-                <ToastContainer
-                    position="top-right"
-                    autoClose={2000}
-                    limit={2}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="colored"
-                    transition={Slide}
-                />
+            <div>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/dashboard" element={<Dashboard />} />
