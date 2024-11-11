e7d863a4f589ff9a6daa30645a6f481d762e98b2
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2024-11-11 23:44:22 +0530

    minor bug fix

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -4,7 +4,9 @@ import { Card } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
+import { MapPin, MessageCircle, Phone, Send } from "lucide-react"
 import { useState } from "react"
 
 export default function Component() {
@@ -12,6 +14,7 @@ export default function Component() {
         firstName: '',
         lastName: '',
         email: '',
+        phone: '',
         message: '',
         services: []
     })
